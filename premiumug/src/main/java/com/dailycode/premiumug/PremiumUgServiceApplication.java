9447b1cbb64b72e5989678ddadee553e4d9e1d23
Eric Muiruri <eric.muiruri@jubileekenya.com>
2024-03-04 16:09:59 +0300

    UG- Premium process

---
+++ premiumug/src/main/java/com/dailycode/premiumug/PremiumUgServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dailycode.premiumug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremiumUgServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremiumUgServiceApplication.class, args);
+	}
+
+}
---
+++ premiumug/src/test/java/com/dailycode/premiumug/PremiumUgServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycode.premiumug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremiumUgServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
